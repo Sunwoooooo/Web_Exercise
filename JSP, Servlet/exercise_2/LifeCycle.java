6cde4d34eecdd0f81def28f573141ac561b04cac
Sunwoo <psw6394@naver.com>
2017-11-20 16:53:59 +0900

    init, destroy와 어노테이션을 이용한 선처리, 후처리 실습

---
+++ JSP, Servlet/exercise_2/LifeCycle.java
@@ -0,0 +1,68 @@
+
+
+import java.io.IOException;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.sun.org.apache.xml.internal.security.Init;
+
+/**
+ * Servlet implementation class LifeCycle
+ */
+@WebServlet("/LifeCycle")
+public class LifeCycle extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LifeCycle() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+    
+    @Override
+    public void init() throws ServletException {
+    	System.out.println("init");
+    }
+    
+    @Override
+    public void destroy() {
+    	System.out.println("destroy");
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("doGet");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("doPost");
+	}
+	
+	@PostConstruct
+	private void initPostConstruct() {
+		// TODO Auto-generated method stub
+		System.out.println("initPostConstruct");
+	}
+	
+	@PreDestroy
+	private void destroyPreDestroy() {
+		// TODO Auto-generated method stub
+		System.out.println("destroyPreDestroy");
+	}
+}
